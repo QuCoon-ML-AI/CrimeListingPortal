5bebbdd7e5f301e805b6f81f13957d766761e2ae
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 03:35:27 +0100

    Initial commit for CrimeListingPortal

---
+++ src/main/kotlin/com/pwb/crimeListingPortal/utility/constant/PrefixConstant.kt
@@ -0,0 +1,6 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T02:34:20.323740600
+package com.pwb.crimeListingPortal.utility.constant
+
+object PrefixConstant {
+    const val ENTITY = "DBO."
+}
