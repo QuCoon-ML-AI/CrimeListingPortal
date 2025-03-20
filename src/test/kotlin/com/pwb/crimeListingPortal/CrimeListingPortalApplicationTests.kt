65dd88154518b06eb20f96cce04608e4348e58e8
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 03:35:35 +0100

    Initial commit for CrimeListingPortal

---
+++ src/test/kotlin/com/pwb/crimeListingPortal/CrimeListingPortalApplicationTests.kt
@@ -0,0 +1,15 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T02:34:20.370206800
+package com.pwb.crimeListingPortal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrimeListingPortalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
