8f15c75465bd501d0db6f3fd8e14c4050c9ebd12
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 03:34:41 +0100

    Initial commit for CrimeListingPortal

---
+++ src/main/kotlin/com/pwb/crimeListingPortal/CrimeListingPortalApplication.kt
@@ -0,0 +1,13 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T02:34:20.323740600
+package com.pwb.crimeListingPortal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrimeListingPortalApplication
+
+fun main(args: Array<String>) {
+	runApplication< CrimeListingPortalApplication>(*args)
+}
+
